6b5794352980cb59ea5487d09471b510fad2f848
victor <victor.malassigne@inserm.fr>
2023-01-31 16:20:57 +0100

    ajout V4

--- script/10_freq_PC1_et_joursprelevement.R
+++ script/10_freq_PC1_et_joursprelevement.R
@@ -12,6 +12,8 @@ library(openxlsx)
 # 2 Import data-----------------------------------------------------------------
 rm(list = ls())
 data_V1 <- read_xlsx("/Users/victor/Documents/JM/NanoString/Data_brut/Covid_IFN-avec_data_V5.xlsx",6)
+data_V4_CD4_8_Agspe <- read_xlsx("/Users/victor/Documents/JM/NanoString/IFN_covid_nano/data/nano_freq_V4_T_cell_omiq.xlsx",2)
+data_V4_T_CD4_8<- read_xlsx("/Users/victor/Documents/JM/NanoString/IFN_covid_nano/data/nano_freq_V4_T_cell_omiq.xlsx",1)
 data_6m <- read_xlsx("/Users/victor/Documents/JM/NanoString/Data_brut/Covid_IFN-avec_data_V5.xlsx",8)
 PC1_V1 <- read.table("/Users/victor/Documents/JM/NanoString/IFN_covid_nano/data/PC1_V1_gene_DE_NRvsR.txt")
 load("data/1.3_mat_pat_clean_final.rds") #ouverture de la svg
@@ -157,8 +159,210 @@ rownames(dat) <- colnames(matcor)
 colnames(dat) <- c("p-val", "cor")
 write.xlsx(dat, file = "result/correlation_pvalue_main_subset_V1.xlsx", rowNames = T)
 
-# 4 Frequence cell 6 months-----------------------------------------------------
-## 4.1 Freq vs jour de prelevement----------------------------------------------
+
+# 4 Frequence cell V4 CD4-8 Ag spe----------------------------------------------
+## 4.2 Freq vs PCA--------------------------------------------------------------
+data_V4_CD4_8_Agspe <- data_V4_CD4_8_Agspe[data_V4_CD4_8_Agspe$numero_patient != 50,] ## supression du patient 50
+
+data_fi <- inner_join(data_V4_CD4_8_Agspe, PC1_V1, by = c("numero_patient" = "numero_patient",
+                                              "Responder"="REPONSE")) %>% 
+  filter(Responder != "T") %>%
+  rename(numero_patient = "numero_patient") %>%
+  mutate(label_patient = as.character(numero_patient)) %>%
+  arrange(numero_patient)
+
+setdiff(data_V4_CD4_8_Agspe$numero_patient, data_fi$numero_patient)
+##  manque 56 64 66 normale car un seul prelevement
+
+namevars_to_plot <- names(data_fi[9:40])
+
+list_plot <- list()
+for (namevar_i in namevars_to_plot) {
+  print(namevar_i)
+  p_i <- ggplot(
+    data_fi, aes(x = SelectPCA.PC1 , y = .data[[namevar_i]], color = Responder, label = label_patient)) +
+    geom_point()+
+    ggrepel::geom_text_repel(show.legend = F,
+                             max.overlaps  = Inf)+
+    scale_color_manual(breaks = c("NR", "NR-", "R", "RP-", "RP", "T", "A"),
+                       values = c("darkorange", "#DDCC77","cornflowerblue",
+                                  "#882255" ,"brown3", "chartreuse4", "#BBBBBB")) 
+  labs(title=namevar_i) +
+    theme_bw()
+  list_plot[[namevar_i]] <- p_i
+  
+}
+
+plot_grid(plotlist = list_plot[1:6], ncol = 2) # pour orga ne nombre de plot sur la page
+
+p_all = plot_grid(plotlist = list_plot, ncol = 3)
+ggsave(filename = "result/V4_CD4_8_Agspe_non_supervise_sans50.pdf", plot = p_all, width = 20, height = 35, limitsize = F )
+
+### 4.2.1 V4 correlation-------------------------------------------------------
+### Calcule des correlations & save ###
+list_cor_pval <- list()
+for (namevar_i in namevars_to_plot) {
+  print(namevar_i)
+  data_clean <- as.data.frame(data_fi[namevar_i])
+  data_clean$SelectPCA.PC1 <- data_fi$SelectPCA.PC1
+  data_clean <- na.omit(data_clean)
+  
+  p_i <- cor.test(data_clean$SelectPCA.PC1, data_clean[[namevar_i]], method= "pearson")
+  list_cor_pval[[namevar_i]] <- p_i
+}
+
+dat <-  NULL
+for (i in list_cor_pval) {
+  test <- cbind(as.data.frame(i$p.value),as.data.frame(i$estimate))
+  dat <- rbind(dat, test)
+}
+rownames(dat) <- namevars_to_plot
+colnames(dat) <- c("p-val", "cor")
+write.xlsx(dat, file = "result/correlation_pvalue_V4_CD4_8_Agspe_non_supervise_sans50.xlsx", rowNames = T)
+
+
+# 5 Frequence cell V4 CD4-8 ----------------------------------------------------
+## 5.1 V4 T CD4 CD8 totaux------------------------------------------------------
+data_V4_T_CD4_8[,9:62]<- lapply(data_V4_T_CD4_8[,9:62], as.numeric) 
+
+data_V4_T_CD4_8 <- data_V4_T_CD4_8[data_V4_T_CD4_8$numero_patient != 50,] ## supprime le patient 50
+
+data_True_false <- data_V4_T_CD4_8$Stimulation %in% "no stim"
+data_V4_T_CD4_8_no_stim <- data_V4_T_CD4_8[data_True_false==T,]
+data_V4_T_CD4_8_stim <- data_V4_T_CD4_8[data_True_false==F,]
+
+## 5.2 Freq vs PCA non stim-----------------------------------------------------
+data_fi <- inner_join(data_V4_T_CD4_8_no_stim, PC1_V1, by = c("numero_patient" = "numero_patient",
+                                                          "Responder"="REPONSE")) %>% 
+  filter(Responder != "T") %>%
+  rename(numero_patient = "numero_patient") %>%
+  mutate(label_patient = as.character(numero_patient)) %>%
+  arrange(numero_patient)
+
+setdiff(data_V4_T_CD4_8_no_stim$numero_patient, data_fi$numero_patient)
+##  manque 56 64 66 normale car un seul prelevement
+
+namevars_to_plot <- names(data_fi[9:62])
+
+list_plot <- list()
+for (namevar_i in namevars_to_plot) {
+  print(namevar_i)
+  # suppression des NA dans la variable
+  data_clean <- as.data.frame(data_fi[namevar_i])
+  data_clean$Responder <- data_fi$Responder
+  data_clean$label_patient <- data_fi$label_patient
+  data_clean$SelectPCA.PC1 <- data_fi$SelectPCA.PC1
+  data_clean <- na.omit(data_clean)
+  # plot 
+  p_i <- ggplot(
+    data_clean, aes(x = SelectPCA.PC1, y = .data[[namevar_i]], color = Responder, label = label_patient)) +
+    geom_point()+
+    ggrepel::geom_text_repel(show.legend = F,
+                             max.overlaps  = Inf)+
+    scale_color_manual(breaks = c("NR", "NR-", "R", "RP-", "RP", "T", "A"),
+                       values = c("darkorange", "#DDCC77","cornflowerblue",
+                                  "#882255" ,"brown3", "chartreuse4", "#BBBBBB")) 
+  labs(title=namevar_i) +
+    theme_bw()
+  list_plot[[namevar_i]] <- p_i
+  
+}
+
+plot_grid(plotlist = list_plot[1:6], ncol = 2) # pour orga ne nombre de plot sur la page
+
+p_all = plot_grid(plotlist = list_plot, ncol = 3)
+ggsave(filename = "result/V4_CD4_8_non_supervise_no_stim_sans50.pdf", plot = p_all, width = 20, height = 50, limitsize = F )
+
+### 5.2.1 V4 non stim correlation-----------------------------------------------
+### Calcule des correlations & save ###
+list_cor_pval <- list()
+for (namevar_i in namevars_to_plot) {
+  print(namevar_i)
+  data_clean <- as.data.frame(data_fi[namevar_i])
+  data_clean$SelectPCA.PC1 <- data_fi$SelectPCA.PC1
+  data_clean <- na.omit(data_clean)
+  
+  p_i <- cor.test(data_clean$SelectPCA.PC1, data_clean[[namevar_i]], method= "pearson")
+  list_cor_pval[[namevar_i]] <- p_i
+}
+
+dat <-  NULL
+for (i in list_cor_pval) {
+  test <- cbind(as.data.frame(i$p.value),as.data.frame(i$estimate))
+  dat <- rbind(dat, test)
+}
+rownames(dat) <- namevars_to_plot
+colnames(dat) <- c("p-val", "cor")
+write.xlsx(dat, file = "result/correlation_pvalue_V4_CD4_8_non_supervise_no_stim_sans50.xlsx", rowNames = T)
+
+
+## 5.2 Freq vs PCA stim---------------------------------------------------------
+data_fi <- inner_join(data_V4_T_CD4_8_stim, PC1_V1, by = c("numero_patient" = "numero_patient",
+                                                              "Responder"="REPONSE")) %>% 
+  filter(Responder != "T") %>%
+  rename(numero_patient = "numero_patient") %>%
+  mutate(label_patient = as.character(numero_patient)) %>%
+  arrange(numero_patient)
+
+setdiff(data_V4_T_CD4_8_stim$numero_patient, data_fi$numero_patient)
+##  manque 56 64 66 normale car un seul prelevement
+
+namevars_to_plot <- names(data_fi[9:62])
+
+list_plot <- list()
+for (namevar_i in namevars_to_plot) {
+  print(namevar_i)
+  # suppression des NA dans la variable
+  data_clean <- as.data.frame(data_fi[namevar_i])
+  data_clean$Responder <- data_fi$Responder
+  data_clean$label_patient <- data_fi$label_patient
+  data_clean$SelectPCA.PC1 <- data_fi$SelectPCA.PC1
+  data_clean <- na.omit(data_clean)
+  # plot 
+  p_i <- ggplot(
+    data_clean, aes(x = SelectPCA.PC1, y = .data[[namevar_i]], color = Responder, label = label_patient)) +
+    geom_point()+
+    ggrepel::geom_text_repel(show.legend = F,
+                             max.overlaps  = Inf)+
+    scale_color_manual(breaks = c("NR", "NR-", "R", "RP-", "RP", "T", "A"),
+                       values = c("darkorange", "#DDCC77","cornflowerblue",
+                                  "#882255" ,"brown3", "chartreuse4", "#BBBBBB")) 
+  labs(title=namevar_i) +
+    theme_bw()
+  list_plot[[namevar_i]] <- p_i
+  
+}
+
+plot_grid(plotlist = list_plot[1:6], ncol = 2) # pour orga ne nombre de plot sur la page
+
+p_all = plot_grid(plotlist = list_plot, ncol = 3)
+ggsave(filename = "result/V4_CD4_8_non_supervise_stim_sans50.pdf", plot = p_all, width = 20, height = 50, limitsize = F )
+
+### 5.2.1 V4 stim correlation---------------------------------------------------
+### Calcule des correlations & save ###
+list_cor_pval <- list()
+for (namevar_i in namevars_to_plot) {
+  print(namevar_i)
+  data_clean <- as.data.frame(data_fi[namevar_i])
+  data_clean$SelectPCA.PC1 <- data_fi$SelectPCA.PC1
+  data_clean <- na.omit(data_clean)
+  
+  p_i <- cor.test(data_clean$SelectPCA.PC1, data_clean[[namevar_i]], method= "pearson")
+  list_cor_pval[[namevar_i]] <- p_i
+}
+
+dat <-  NULL
+for (i in list_cor_pval) {
+  test <- cbind(as.data.frame(i$p.value),as.data.frame(i$estimate))
+  dat <- rbind(dat, test)
+}
+rownames(dat) <- namevars_to_plot
+colnames(dat) <- c("p-val", "cor")
+write.xlsx(dat, file = "result/correlation_pvalue_V4_CD4_8_non_supervise_stim_sans50.xlsx", rowNames = T)
+
+
+# 6 Frequence cell 6 months-----------------------------------------------------
+## 6.1 Freq vs jour de prelevement----------------------------------------------
 # ma <- mat_pat_clean$time_point %in% "V1"
 # mat <- mat_pat_clean[ma == T,]
 # mat[,1:736] <- NULL
@@ -221,7 +425,7 @@ write.xlsx(dat, file = "result/correlation_pvalue_main_subset_V1.xlsx", rowNames
 # p_all = plot_grid(plotlist = list_plot, ncol = 3)
 # ggsave(filename = "result/6_months_V1_jour_prelevement_T_sans50.pdf", plot = p_all, width = 20, height = 100, limitsize = F )
 # 
-# ### 4.1.1 VT1 correlation-------------------------------------------------------
+# ### 6.1.1 VT1 correlation-------------------------------------------------------
 # ### correlation with p-value ###
 # list_cor_pval <- list()
 # for (namevar_i in namevars_to_plot) {
@@ -246,7 +450,7 @@ write.xlsx(dat, file = "result/correlation_pvalue_main_subset_V1.xlsx", rowNames
 # colnames(dat) <- c("p-val", "cor")
 # write.xlsx(dat, file = "result/correlation_pvalue_6_months_V1_jour_prelevement_T.xlsx", rowNames = T)
 
-## 4.2 Freq vs PCA--------------------------------------------------------------
+## 6.2 Freq vs PCA--------------------------------------------------------------
 data_fi <- inner_join(data_6m, PC1_V1, by = c("numero_patient_nanostring" = "numero_patient",
                                               "REPONSE"="REPONSE")) %>%
   filter(REPONSE != "T") %>%
@@ -294,7 +498,7 @@ plot_grid(plotlist = list_plot[1:6], ncol = 2) # pour orga ne nombre de plot sur
 p_all = plot_grid(plotlist = list_plot, ncol = 3)
 ggsave(filename = "result/6_months_V1_APC_sans50.pdf", plot = p_all, width = 20, height = 100, limitsize = F )
 
-### 4.2.1 VT1 correlation-------------------------------------------------------
+### 6.2.1 6 months correlation-------------------------------------------------------
 ### correlation with p-value ###
 list_cor_pval <- list()
 for (namevar_i in namevars_to_plot) {
